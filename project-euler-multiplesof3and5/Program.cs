a0723db24d70a1a2ba6e7e70b416e6c93d46cf77
Floohp <Floohp@DESKTOP-S85HFGV>
2021-06-07 21:53:36 +0200

    commit?

--- project-euler-multiplesof3and5/Program.cs
+++ project-euler-multiplesof3and5/Program.cs
@@ -6,7 +6,16 @@ namespace project_euler_multiplesof3and5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int total = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    total = total + i;
+                    Console.WriteLine(total);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }

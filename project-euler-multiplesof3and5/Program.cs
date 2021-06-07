091ffd5cb3b1cf22b250f202a1958d6b66bbf205
Floohp <Floohp@DESKTOP-S85HFGV>
2021-06-07 21:10:44 +0200

    Add project files.

---
+++ project-euler-multiplesof3and5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_euler_multiplesof3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
